6d1718de7a7f8b52efc9e61adf4d2566431a2ec7
Gireesh Purupuruthan <gireesh971@gmail.com>
2018-04-03 08:29:27 -0500

    Log all out puts to file

--- aws-nodes/scripts/bastion-init.sh
+++ aws-nodes/scripts/bastion-init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-echo "Starting the init script execution" >> /home/ubuntu/init-trail.log
-date  >> /home/ubuntu/init-trail.log
+
+# Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
+exec > >(tee -i /home/ubuntu/init-trail.log)
+exec 2>&1
+
+echo "Starting the init script execution"
+date
 cd /home/ubuntu
 git clone https://github.com/gireesh971/refer-arch-aws.git
 cd /home/ubuntu/refer-arch-aws/aws-nodes
@@ -10,5 +15,5 @@ sudo apt-get update
 sudo apt-get install tinyproxy
 sudo cp tinyproxy.conf /etc/tinyproxy.conf
 sudo /etc/init.d/tinyproxy restart
-echo "Completed the init script execution" >> /home/ubuntu/init-trail.log
-date  >> /home/ubuntu/init-trail.log
+echo "Completed the init script execution"
+date
