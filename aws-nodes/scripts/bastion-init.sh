fb1be30234611d89b9ed097f7dad23c70d893f7b
Gireesh Purupuruthan <gireesh971@gmail.com>
2018-04-03 08:05:24 -0500

    update script

--- aws-nodes/scripts/bastion-init.sh
+++ aws-nodes/scripts/bastion-init.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cat "ran the script" | ~/out.txt
-
-
+echo "Starting the init script execution" >> /home/ubuntu/init-trail.log
+date  >> /home/ubuntu/init-trail.log
+cd /home/ubuntu
+git clone https://github.com/gireesh971/refer-arch-aws.git
+git pull
+cd /home/ubuntu/refer-arch-aws/aws-nodes/scripts/
+#chmod +x bastion-init.sh
+#./bastion-init.sh
+echo "Completed the init script execution" >> /home/ubuntu/init-trail.log
+date  >> /home/ubuntu/init-trail.log
