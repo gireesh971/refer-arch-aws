53095bd2bcae6a7a67a43a562e4f5a2943f1cf93
Gireesh Purupuruthan <gireesh971@gmail.com>
2018-04-03 08:08:06 -0500

    Fix errors

--- aws-nodes/scripts/bastion-init.sh
+++ aws-nodes/scripts/bastion-init.sh
@@ -3,6 +3,7 @@ echo "Starting the init script execution" >> /home/ubuntu/init-trail.log
 date  >> /home/ubuntu/init-trail.log
 cd /home/ubuntu
 git clone https://github.com/gireesh971/refer-arch-aws.git
+cd /home/ubuntu/refer-arch-aws/aws-nodes
 git pull
 cd /home/ubuntu/refer-arch-aws/aws-nodes/scripts/
 #chmod +x bastion-init.sh
