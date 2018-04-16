5ff581e7564de079524b2e959e23cb2533c100b0
Gireesh Purupuruthan <gireesh971@gmail.com>
2018-04-15 19:55:22 -0500

    Copying of the web apps

--- apps/deploy.sh
+++ apps/deploy.sh
@@ -16,3 +16,5 @@ cd ./dist
 zip -r apps.zip apps/*
 scp -i ~/.aws/ec2-keypair-1.pem ./apps.zip ubuntu@$1:/home/ubuntu
 cd ..
+scp -i ~/.aws/ec2-keypair-1.pem dist/apps/admin/* ubuntu@$1:/var/www/html/apps/admin
+scp -i ~/.aws/ec2-keypair-1.pem dist/apps/login/* ubuntu@$1:/var/www/html/apps/login
